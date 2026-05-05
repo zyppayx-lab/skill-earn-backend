fd91476e07e0521b603b23130d056773d35027c2
zyppayx-lab <zyppayx@gmail.com>
2026-05-06 06:30:00 +0800

    Delete src directory

--- src/config/env.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  port: process.env.PORT,
-  jwtSecret: process.env.JWT_SECRET,
-  paystackKey: process.env.PAYSTACK_SECRET_KEY,
-  resendKey: process.env.RESEND_API_KEY,
-  appName: process.env.APP_NAME
-};
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const { port } = require("./config/env");
-
-app.listen(port, () => {
-  console.log(`SkillEarn running on port ${port}`);
-});
