bbe0a5b6053ef7d82c02ebe2334ffdfb1f4f597f
zyppayx-lab <zyppayx@gmail.com>
2026-05-06 06:33:57 +0800

    Add generateToken function for JWT creation

---
+++ src/utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (user) => {
+  return jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
